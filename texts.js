7129d1d2d99f4947b16256afe21e1396573f4f6b
Tristan S <tschwichow@gmail.com>
2022-04-14 12:26:30 -0700

    added random texts

---
+++ texts.js
@@ -0,0 +1,115 @@
+const randomPlaintexts = [
+    `We're no strangers to love
+    You know the rules and so do I
+    A full commitment's what I'm thinking of
+    You wouldn't get this from any other guy
+    
+    I just wanna tell you how I'm feeling
+    Gotta make you understand
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you
+    
+    We've known each other for so long
+    Your heart's been aching, but
+    You're too shy to say it
+    Inside, we both know what's been going on
+    We know the game and we're gonna play it
+    
+    And if you ask me how I'm feeling
+    Don't tell me you're too blind to see
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you
+    
+    (Ooh, give you up)
+    (Ooh, give you up)
+    Never gonna give, never gonna give
+    (Give you up)
+    Never gonna give, never gonna give
+    (Give you up)
+    
+    We've known each other for so long
+    Your heart's been aching, but
+    You're too shy to say it
+    Inside, we both know what's been going on
+    We know the game and we're gonna play it
+    
+    I just wanna tell you how I'm feeling
+    Gotta make you understand
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you
+    
+    Never gonna give you up
+    Never gonna let you down
+    Never gonna run around and desert you
+    Never gonna make you cry
+    Never gonna say goodbye
+    Never gonna tell a lie and hurt you`,
+
+    `Twinkle, twinkle, little starHow I wonder what you areUp above the world so highLike a diamond in the skyTwinkle, twinkle, little starHow I wonder what you areTwinkle, twinkle, little starHow I wonder what you areUp above the world so highLike a diamond in the skyTwinkle, twinkle, little starHow I wonder what you areTwinkle, twinkle, little starHow I wonder what you areUp above the world so highLike a diamond in the skyTwinkle, twinkle, little starHow I wonder what you areTwinkle, twinkle, little starHow I wonder what you areUp above the world so highLike a diamond in the skyTwinkle, twinkle, little starHow I wonder what you are`,
+    
+    `Oh, say can you see by the dawn's early light
+    What so proudly we hailed at the twilight's last gleaming?
+    Whose broad stripes and bright stars thru the perilous fight,
+    O'er the ramparts we watched were so gallantly streaming?
+    And the rocket's red glare, the bombs bursting in air,
+    Gave proof through the night that our flag was still there.
+    Oh, say does that star-spangled banner yet wave
+    O'er the land of the free and the home of the brave?
+    
+    On the shore, dimly seen through the mists of the deep,
+    Where the foe's haughty host in dread silence reposes,
+    What is that which the breeze, o'er the towering steep,
+    As it fitfully blows, half conceals, half discloses?
+    Now it catches the gleam of the morning's first beam,
+    In full glory reflected now shines in the stream:
+    ‘Tis the star-spangled banner! Oh long may it wave
+    O'er the land of the free and the home of the brave!
+    
+    And where is that band who so vauntingly swore
+    That the havoc of war and the battle's confusion,
+    A home and a country should leave us no more!
+    Their blood has washed out their foul footsteps' pollution.
+    No refuge could save the hireling and slave
+    From the terror of flight, or the gloom of the grave:
+    And the star-spangled banner in triumph doth wave
+    O'er the land of the free and the home of the brave!
+    
+    Oh! thus be it ever, when freemen shall stand
+    Between their loved home and the war's desolation!
+    Blest with victory and peace, may the heav'n rescued land
+    Praise the Power that hath made and preserved us a nation.
+    Then conquer we must, when our cause it is just,
+    And this be our motto: “In God is our trust.”
+    And the star-spangled banner in triumph shall wave
+    O'er the land of the free and the home of the brave!`
+
+]
